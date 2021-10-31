a52fa74139f44a0a1b70d33a16d4c7ca71fd388b
MashodRana <mashod0rana@gmail.com>
2021-11-01 03:18:47 +0600

    header updated.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -32,23 +32,23 @@ const Header = () => {
                         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
                     </button>
                     <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-                        <li class=""><NavLink to='/add-plan' class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Add Plan</NavLink></li>
+                        {user.email && <li class=""><NavLink to='/add-plan' class="rounded-t bg-gray-200 hover:bg-red-400 py-2 px-4 block whitespace-no-wrap">Add Plan</NavLink></li>}
                         {user.email && <li >
-                            <NavLink to='/my-plans' class="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
+                            <NavLink to='/my-plans' class="rounded-t bg-gray-200 hover:bg-red-400 py-2 px-4 block whitespace-no-wrap">
                                 My Plans
                             </NavLink>
                         </li>}
                         {user.email && <li >
-                            <NavLink to='/manage-all-plans' class="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
+                            <NavLink to='/manage-all-plans' class="bg-gray-200 hover:bg-red-400 py-2 px-4 block whitespace-no-wrap">
                                 Manage All Plans
                             </NavLink>
                         </li>}
                         {
                             user.email ? <li><span
                                 onClick={logOut}
-                                class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
+                                class="rounded-b bg-gray-200 hover:bg-red-400 py-2 px-4 block whitespace-no-wrap"
                             >Logout</span></li>
-                                : <li><NavLink to='/login' class="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">Login/Register
+                                : <li><NavLink to='/login' class="rounded-b bg-gray-200 hover:bg-red-400 py-2 px-4 block whitespace-no-wrap">Login/Register
                                 </NavLink></li>
                         }
 
