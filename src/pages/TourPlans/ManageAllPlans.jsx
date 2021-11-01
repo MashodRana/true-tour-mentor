9c7a4abb640f4bff75634074e8f265c1c6302541
MashodRana <mashod0rana@gmail.com>
2021-11-01 17:52:40 +0600

    status update done.

--- src/pages/TourPlans/ManageAllPlans.jsx
+++ src/pages/TourPlans/ManageAllPlans.jsx
@@ -2,57 +2,95 @@ import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 
 const ManageAllPlans = () => {
-    const [allPlans, setAllPlans] = useState(null);
-    const [message, setMessage] = useState(null);
-    const { user } = useAuth();
+  const [allPlans, setAllPlans] = useState(null);
+  const [message, setMessage] = useState(null);
+  const { user } = useAuth();
 
-    useEffect(() => {
-        const url = `https://shrieking-spider-08076.herokuapp.com/all-orders`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => setAllPlans(data))
-    }, [])
+  useEffect(() => {
+    const url = `https://shrieking-spider-08076.herokuapp.com/all-orders`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setAllPlans(data));
+  }, []);
 
-    const deletePlan = (planId) => {
-        const flag = window.confirm("Are you sure you want to cancel your our plan");
-        if (flag) {
-            const url = `https://shrieking-spider-08076.herokuapp.com/remove-order/${planId}`;
-            fetch(url, {
-                method: 'DELETE',
-            })
-                .then(res => res.json())
-                .then(data => {
-                    const newPlans = allPlans.filter(plan => plan._id !== planId);
-                    setAllPlans(newPlans);
-                    setMessage(data.message);
-                    alert(message);
-                })
-        }
-
-    }
-    return (
-        <main className="container mx-auto">
-            <section>
-                <h1 className="text-3xl">Manage All Plans</h1>
-                <div>
-                    {
-                        allPlans ? allPlans.map(plan => <>
-                            <h4 className="text-xl">{plan.title}</h4>
-                            <p>Status: {plan.orderStatus}</p>
-                            <p>Number of People: {plan.numPeople}</p>
-                            <p>Prefered Date: {plan.prefDate}</p>
-                            <p>Cost: {plan.cost}</p>
-                            <p>Total Cost: {plan.cost * plan.numPeople}</p>
-                            <div>
-                                <button className="border p-2" onClick={() => deletePlan(plan._id)}>Cancel Plan</button>
-                            </div>
-                        </>)
-                            : <p>There are no plans.</p>
-                    }
-                </div>
-            </section>
-        </main>
+  const deletePlan = (planId) => {
+    const flag = window.confirm(
+      "Are you sure you want to cancel your our plan"
     );
+    if (flag) {
+      const url = `https://shrieking-spider-08076.herokuapp.com/remove-order/${planId}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const newPlans = allPlans.filter((plan) => plan._id !== planId);
+          setAllPlans(newPlans);
+          setMessage(data.message);
+          alert(message);
+        });
+    }
+  };
+
+  const processPlan = (id)=>{
+      const url = `http://localhost:5000/update-order-status/${id}`;
+      fetch(url, {
+          method:'PUT'
+      })
+      .then(res=>res.json())
+      .then(data=>{
+          alert(data.message);
+          window.location.reload();
+      })
+  }
+
+  return (
+    <main className="container lg:w-11/12 mx-auto">
+      <section className="my-16">
+        <h1 className="text-3xl mb-8">Manage All Plans</h1>
+        <div>
+          <div class="grid md:grid-cols-3 grid-cols-1 lg:gap-6 md:gap-4">
+            {allPlans ? (
+              allPlans.map((plan) => (
+                <>
+                  <div className="shadow-md p-4">
+                    <h4 className="text-xl">{plan.title}</h4>
+                    <p className="text-gray-400 text-sm">
+                      Number of People: {plan.numPeople}
+                    </p>
+                    <p className="text-gray-400 text-sm">
+                      Prefered Date: {plan.prefDate}
+                    </p>
+                    <p className="text-gray-400 text-sm">Cost: {plan.cost}</p>
+                    <p className="text-gray-400 text-sm">
+                      Total Cost: {plan.cost * plan.numPeople}
+                    </p>
+                    <p className="text-gray-400 text-sm">Status: <span className="bg-yellow-300 rounded border text-black">{plan.orderStatus}</span></p>
+                    <div className="mt-6">
+                      <button 
+                      className="py-1 px-2 rounded border mr-4 border border-green-400 text-green-600 hover:bg-green-600 hover:text-white"
+                      onClick = {()=>processPlan(plan._id)}
+                      >
+                        Process Plan
+                      </button>
+                      <button
+                        className="py-1 px-2 rounded border border-red-400 text-red-400 hover:bg-red-400 hover:text-white"
+                        onClick={() => deletePlan(plan._id)}
+                      >
+                        Cancel Plan
+                      </button>
+                    </div>
+                  </div>
+                </>
+              ))
+            ) : (
+              <p>There are no plans.</p>
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default ManageAllPlans;
